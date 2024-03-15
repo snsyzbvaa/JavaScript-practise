be12ed70e12d22fe3b62bd56980b78c73c52543c
snsyzbvaa <eldanasansyzbaevaa06gmail.com>
2024-03-16 02:07:57 +0600

    push

---
+++ practise3.js
@@ -0,0 +1,78 @@
+// const now = new Date()
+
+// const start = new Date(1000 * 60 * 60 * 24 * 365)
+
+// const date = new Date(2011, 0, 4, 12, 42, 12)
+
+// console.log(now.getFullYear())
+// console.log(now.getMonth())
+// console.log(now.getDate())
+// console.log(now.getHours())
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+// console.log(now.getMilliseconds())
+
+// now.setFullYear(2055)
+
+// console.log(now.toDateString())
+// console.log(now.toTimeString())
+// console.log(now.toLocaleDateString())
+// console.log(now.toLocaleTimeString())
+
+let mode = 'full';
+const output = document.getElementById('output');
+const fullBtn = document.getElementById('full');
+const dateBtn = document.getElementById('date');
+const timeBtn = document.getElementById('time');
+
+function bindMode(name) {
+    return function() {
+        mode = name;
+        update();
+    };
+};
+
+fullBtn.onclick = function() {
+    mode = 'full';
+    update();
+};
+
+dateBtn.onclick = function () {
+    mode = 'date';
+    update();
+};
+
+fullBtn.onclick = bindMode('full');
+
+dateBtn.onclick = bindMode('date');
+
+timeBtn.onclick = bindMode('time');
+
+setInterval(update, 1000);
+update();
+
+function update() {
+    output.textContent = format(mode);
+};
+
+// output.textContent = new Date()
+
+// setInterval(() => {
+//     output.textContent = format(mode)
+// }, 1000)
+
+function format(formatMode) {
+    const now = new Date();
+
+    switch (formatMode) {
+        case 'time':
+            return now.toLocaleTimeString() + '.' + now.getMilliseconds();
+        case 'date':
+            return now.toLocaleDateString();
+        case 'full':
+            return now.toLocaleDateString() + ' ' + now.toLocaleDateString();
+        default:
+            return now.toLocaleDateString();
+    };
+
+};
